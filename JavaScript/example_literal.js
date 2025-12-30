486a10821526edbbb1056e8546e7bff93d55453e
NeoNexus <NeoNexus@github.com>
2025-12-30 22:02:09 +0800

    添加示例代码：字面量和变量类型

---
+++ JavaScript/example_literal.js
@@ -0,0 +1,16 @@
+//1 Literal String 
+console.log("Hello, World!");
+//2 Literal Number
+console.log(1);
+//3 Literal Boolean
+console.log(true);
+//4 Null Literal
+console.log(null);
+//5 Literal regular expression
+console.log(/ab+c/);
+//6 Literal Array
+console.log([1, 2, 3]);
+//7 Literal Dictionary (Object)
+console.log({name: "John", age: 30});
+//8 function Literal
+console.log(function() { return "Hello"; });
---
+++ JavaScript/example_variable.js
@@ -0,0 +1,17 @@
+// Number type variable
+var num = 42;
+console.log(num);
+var floatNum = 3.14;
+console.log(floatNum);
+
+//binary type variable
+var binNum = 0b1010; // binary for 10
+console.log(binNum);
+
+//octal type variable
+var octNum = 0o52; // octal for 42
+console.log(octNum);
+
+//hexadecimal type variable
+var hexNum = 0x2A; // hexadecimal for 42
+console.log(hexNum);
